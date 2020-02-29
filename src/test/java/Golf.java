82bebdff23240560ad4d254a959c8fb4b28fb96a
wugi <17031278@itcelaya.edu.mx>
2020-02-29 17:27:02 -0600

    Prueba xdddd

---
+++ src/test/java/Golf.java
@@ -0,0 +1,22 @@
+public class Golf
+{
+    private int[] hoyo = new int[18];
+    private int hoyoActual = 0;
+
+    public void hoyo(int tiros)
+    {
+        hoyo[hoyoActual++] = tiros;
+    }
+
+    public int puntuacion()
+    {
+        int puntuacion = 0;
+
+        for (int i = 0; i < 18; i++)
+        {
+            puntuacion += hoyo[i];
+        }
+
+        return puntuacion;
+    }
+}
---
+++ src/test/java/GolfTest.java
@@ -0,0 +1,33 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class GolfTest
+{
+    private Golf golf;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        golf = new Golf();
+    }
+
+    @Test
+    public void test() throws Exception
+    {
+        golf.hoyo(1);
+        golf.hoyo(2);
+        assertEquals(3, golf.puntuacion());
+    }
+
+    @Test
+    public void testJuegoPerfecto() throws Exception
+    {
+        for (int i = 0; i < 18; i++)
+        {
+            golf.hoyo(1);
+        }
+
+        assertEquals(18, golf.puntuacion());
+    }
+}
